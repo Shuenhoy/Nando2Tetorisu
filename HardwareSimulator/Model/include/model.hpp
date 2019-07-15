36bb08e4cb9bb08470340502d654a710d11de1e4
Tingyi Feng <872725792@qq.com>
2019-07-15 10:31:10 +0800

    merged (maybe)

--- HardwareSimulator/Model/include/model.hpp
+++ HardwareSimulator/Model/include/model.hpp
@@ -4,19 +4,16 @@
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
-<<<<<<< HEAD
 #include <optional>
 #include <memory>
 #include <regex>
 #include "builder.hpp"
 #include "chip.hpp"
-=======
->>>>>>> origin/master
+
 
 namespace Nando2Tetorisu::HardwareSimulator
 {
 using pin_map = std::unordered_map<std::string, uint32_t>;
-<<<<<<< HEAD
 
 struct path
 {
@@ -67,27 +64,6 @@ private:
     std::string top_name;
 
     std::string hdl_source;
-=======
-class Model
-{
-public:
-    void load_hdl(std::string source);
-    void load_script(std::string source);
-    void open_output(std::string path);
-    void open_compare(std::string path);
-
-    bool next_script();
-    void set_input_pin(std::string pin, uint32_t value);
-
-    std::pair<pin_map::iterator, pin_map::iterator> input_iter();
-    std::pair<pin_map::iterator, pin_map::iterator> internal_iter();
-    std::pair<pin_map::iterator, pin_map::iterator> output_iter();
-
-private:
-    std::unordered_map<std::string, uint32_t>
-        pins_value;
-    std::unordered_set<std::string> input_pins, output_pins, internal_pins;
->>>>>>> origin/master
 };
 } // namespace Nando2Tetorisu::HardwareSimulator
 
--- HardwareSimulator/ViewModel/include/view_model.hpp
+++ HardwareSimulator/ViewModel/include/view_model.hpp
@@ -2,7 +2,6 @@
 #define VIEW_MODEL_HPP_
 
 #include <string>
-<<<<<<< HEAD
 #include <memory>
 #include <chrono>
 #include <thread>
@@ -84,28 +83,6 @@ private:
     Observer *const observer;
     bool error;
     int speed;
-=======
-#include "../../Model/include/model.hpp"
-#include "../../types.hpp"
-namespace Nando2Tetorisu::HardwareSimulator
-{
-
-class ViewModel
-{
-
-private:
-    void on_load_chip(std::string path);
-    void on_load_script(std::string path);
-    void on_change_animate(AnimateType type);
-    void on_change_format(FormatType type);
-    void on_change_view(ViewType type);
-    void on_change_speed(int speed);
-    void on_single_step();
-    void on_run();
-    void on_reset();
-
-public:
->>>>>>> origin/master
 };
 
 } // namespace Nando2Tetorisu::HardwareSimulator
